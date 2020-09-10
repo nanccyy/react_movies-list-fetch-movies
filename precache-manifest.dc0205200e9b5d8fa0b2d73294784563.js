4226eb19c537b382ef4748408c9217535402368a
nanccyy <lidiapriymak@gmail.com>
2020-09-10 15:01:56 +0300

    Updates

---
+++ precache-manifest.dc0205200e9b5d8fa0b2d73294784563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0794fd775645154f20d937975b28ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e09e4135a0b42ed4839",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "72098426891520677b75",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "6e09e4135a0b42ed4839",
+    "url": "./static/js/2.e3ec00b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e3ec00b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72098426891520677b75",
+    "url": "./static/js/main.33dcf9cc.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
